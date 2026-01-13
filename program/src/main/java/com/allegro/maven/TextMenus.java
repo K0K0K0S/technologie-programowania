e92f0affa915032995a1fe81970ed3019f566efa
Marek H. <m46912@gmail.com>
2026-01-13 20:48:53 +0100

    Utworzenie projektu w Mavenie

---
+++ program/src/main/java/com/allegro/maven/TextMenus.java
@@ -0,0 +1,14 @@
+package com.allegro.maven;
+
+public class TextMenus 
+{
+    public String WelcomeText = "Witamy w naszym sklepie!\nWybierz jedną z opcji poniżej, aby kontynuować:";
+    public String MainMenu = "1. Jestem użytownikem\n2. Jestem Kurierem\n3. Jestem administratorem\n4. Wyjście";
+    public String UserMenu = "\n1. Zarejestruj się\n2. Zaloguj się\n3. Powrót do menu głównego";
+    public String CourierMenu = "\n1. Zaloguj się jako kurier\n2. Powrót do menu głównego";
+    public String AdminMenu = "\n1. Zaloguj się jako administrator\n2. Powrót do menu głównego";
+    public String AdminLoginPrompt = "Podaj Kod Dostępu Aministratora, lub wpisz return aby wrócić do Menu Głównego :";
+    public String ErrorInvalidChoice = "Nieprawidłowy wybór, spróbuj ponownie.";
+    public String UserRegistrationPrompt = "Podaj swoje dane, aby się zarejestrować:";
+    public String [] ListaSlowZabronionych = {"Liverpool", " DA Dach", "Salah"};
+}
