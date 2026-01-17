02a73497183f1eb00629391f4df1fdb4db5b0b7c
Marek H. <m46912@gmail.com>
2026-01-17 20:54:04 +0100

    Czesc zmian w UserInterface

--- program/src/main/java/com/allegro/maven/UserInterface.java
+++ program/src/main/java/com/allegro/maven/UserInterface.java
@@ -1,4 +1,9 @@
 package com.allegro.maven;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class UserInterface
@@ -8,6 +13,10 @@ public class UserInterface
     public static String username;
 
     private static String menu = "1) Wyszukaj przedmiot\n2) Wystaw na sprzedaż\n3) Kup\n4) Przeglądaj moje zamowienia i oferty \n5) Wyloguj się";
+    private static String[] categories = {"Elektronika", "Dom i Ogród", "Motoryzacja", "Sport i Turystyka", "Zdrowie i Uroda", "Kultura i Rozrywka", "Książki", "Inne"};
+    private static String[] filters = {"Kategoria", "Cena minimalna", "Cena maksymalna", "Stan przedmiotu", "Podgląd", "Szukaj", "Powrót do menu głównego"};
+    private static String[] states = {"Nowy", "Używany", "Uszkodzony"};
+
     private static void RunClient()
     {
         System.out.println("Witaj "+ username +"! Co chciałbyś dzisiaj zrobić?");
@@ -21,103 +30,66 @@ public class UserInterface
                 case 1:
                 {
                     boolean flag2 = true;
+                    int filter = 0, category = 8, minPrice = 0, maxPrice = 1000000, state = 1;
                     while(flag2)
                     {
-                        System.out.println("Filtry wyszukiwania:\n1)Kategoria\n2)Cena minimalna\n3)Cena maksymalna\n4)Stan przedmiotu\n5)Szukaj\n6)Powrót do menu głównego");
-                        int filter = scanner.nextInt();
+                        System.out.println("Filtry wyszukiwania");
+                        for (int i = 1; i <= filters.length; i++) 
+                        {
+                            System.out.println(i + ") " + filters[i-1]);
+                        }
+                        filter = scanner.nextInt();
                         switch(filter)
                         {
                             case 1:
                             {
-                                System.out.println("Wybierz kategorię przedmiotu\n1)Elektronika\n2)Dom i Ogród\n3)Motoryzacja\n4)Sport i Turystyka\n5)Zdrowie i Uroda\n6)Kultura i Rozrywka\n7)Inne");
-                                int category = scanner.nextInt();
-                                switch(category)
+                                System.out.println("Wybierz kategorię przedmiotu");
+                                for (int i = 1; i <= categories.length; i++)
                                 {
-                                    case 1:
-                                    {
-
-                                    }
-                                    case 2:
-                                    {
-
-                                    }
-                                    case 3:
-                                    {
-
-                                    }
-                                    case 4:
-                                    {
-
-                                    }
-                                    case 5:
-                                    {
-
-                                    }
-                                    case 6:
-                                    {
-
-                                    }
-                                    case 7:
-                                    {
-
-                                    }
-                                    case 8:
-                                    {
-
-                                    }
-                                    case 9:
-                                    {
-                                        
-                                    }
-                                    default:
-                                    {
-                                        System.out.println("Wybierz jedną z podanych opcji");
-                                    }
-
+                                    System.out.println(i + ") " + categories[i-1]);
                                 }
+                                category = scanner.nextInt();
+                                continue;
                             }
                             case 2:
                             {
                                 System.out.println("Podaj minimalną cenę szukanego produktu");
-                                int minPrice = scanner.nextInt();
-                                break;
+                                minPrice = scanner.nextInt();
+                                continue;
                             }
                             case 3:
                             {
                                 System.out.println("Podaj maksymalną cenę szukanego produktu");
-                                int maxPrice = scanner.nextInt();
-                                break;
+                                maxPrice = scanner.nextInt();
+                                continue;
                             }
                             case 4:
                             {
-                                System.out.println("Podaj stan szukanego przedmiotu:\n1)Nowy\n2)Używany\n3)Uszkodzony");
-                                int state = scanner.nextInt();
-                                switch(state)
+                                System.out.println("Podaj stan szukanego przedmiotu:");
+                                for (int i = 1; i <= states.length; i++)
                                 {
-                                    case 1:
-                                    {
-
-                                    }
-                                    case 2:
-                                    {
-
-                                    }
-                                    case 3:
-                                    {
-
-                                    }
-                                    default:
-                                    {
-                                        System.out.println("Wybierz jedną z podanych opcji");
-                                    }
+                                    System.out.println(i + ") " + states[i-1]);
                                 }
-                                break;
+                                state = scanner.nextInt();
+                                continue;
                             }
                             case 5:
                             {
-
+                                System.out.println("Kategoria: " + categories[category-1] + "\nCena minimalna: " + minPrice + "\nCena maksymalna: " + maxPrice + "\nStan: " + states[state-1]);
                             }
                             case 6:
+                            {
+                                try (Connection conn = DriverManager.getConnection("jdbc:mysql://root:oEZEQyzBUNjknQOXuQNiJuUmwhyOLNRK@hopper.proxy.rlwy.net:29959/railway","root","oEZEQyzBUNjknQOXuQNiJuUmwhyOLNRK");
+                                    Statement stmt = conn.createStatement();)
+                                {
+                                    String searchQuery = "SELECT * FROM Items i WHERE Type = " + categories[category-1] + "AND Price > " + minPrice + "AND Price < " + maxPrice + "AND Status = " + states[state-1];
+                                    var rs = stmt.executeQuery(searchQuery);
+                                } catch (SQLException ex) {
+                                    ex.printStackTrace();
+                                }
+                                
+                            }
+                            case 7:
                             {
                                 flag2 = false;
                                 break;
@@ -146,7 +118,7 @@ public class UserInterface
                     while(state > 0)
                     {
                         state = scanner.nextInt();
-                        if(state == 1 || state == 2)
+                        if(state == 1 || state == 2 || state == 3)
                         {
                             
                         }
